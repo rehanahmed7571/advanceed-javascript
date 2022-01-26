e78b63a109aa2e10fdb1e1d53bb79d1c42df5331
rehanahmed7571 <rehankhandokar@gmail.com>
2022-01-26 18:53:08 +0600

    check truthy or falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,17 @@
+//falsy
+//0
+// null
+// NaN
+// Undefined
+// ""
+// false
+//truthy
+// " ", [], {}
+
+let name = [];
+
+if(name){
+    console.log("the condition is true.");
+}else{
+    console.log('the condition is false')
+}
