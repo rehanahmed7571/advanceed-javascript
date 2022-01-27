f41d7f536cda953b82a92d7d2a531c7b25521b56
rehanahmed7571 <rehankhandokar@gmail.com>
2022-01-27 08:18:59 +0600

    null vs undefined

---
+++ undefined-vs-null.js
@@ -0,0 +1,23 @@
+ //let lover;
+
+ //console.log(lover);
+
+//  function add(num1, num2){
+//      console.log(num1 + num2);
+//  }
+
+//  function add(num1, num2){
+//      console.log(num1,num2);
+//  }
+
+//  const friend = {name: 'dude', phone: 2398432}
+
+// const fun = undefined;
+
+
+
+ //null is set
+
+const ages = [1,2,4,6,7]
+ console.log(ages[11]);
+
