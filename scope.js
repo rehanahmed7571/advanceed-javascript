4f7bb7ecde89d35a87c45af47f0ed6cca21c908f
rehanahmed7571 <rehankhandokar@gmail.com>
2022-01-27 11:15:50 +0600

    scope learned

---
+++ scope.js
@@ -0,0 +1,18 @@
+function sum(first,second){
+   let total = first + second;
+    return total
+}
+
+// console.log(total);
+
+console.log(sum(10,20));
+
+// console.log(day);
+// var day = 'friday';
+
+// console.log(day);
+// let day = 'friday';
+
+
+let day = 'friday';
+console.log(day);
